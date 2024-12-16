eb1d71a40e06c08c583238bc8a6308f7038df0dd
CalvinaHarr <calvinaharr@hotmail.com>
2024-12-15 20:43:54 -0500

    outline for next steps

--- content-script.js
+++ content-script.js
@@ -1,18 +1,22 @@
-// Create a new element
+// Create a button in top bar
 let newElement = document.createElement('button');
-//^Creates a new button in the top bar
 newElement.textContent = "Fact Check";
 newElement.style.padding = "10px";
 
+//style the button
+
 //Add event listener
 let listening = false;
+let isClicked = false;
+
 //Makes the speech recognition
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = true;
 recognition.lang = 'en-US';
-//transcript stuff
+
+//transcript that claims will be pulled from
 const transcript = "";
 recognition.onresult = (event) => {
     transcript = Array.from(event.results)
@@ -23,16 +27,28 @@ recognition.onresult = (event) => {
 };
 
 button.addEventListener("click", function() {
-    //record video clip
-    if(!listening) {
-        listening = true;
-        //start recognition process
-        recognition.start();
+    if(isClicked) {
+        //record video clip
+        if(!listening) {
+            listening = true;
+            //start recognition process
+            recognition.start();
+            //change looks
+            this.textContent = '👂';
+        }
+        else {
+            listening = false;
+        }
     }
     else {
-        listening = false;
+        //change looks
+        this.textContent = '🤓';
     }
-    //change button look to stop
-    //when stopped, compile claims
     alert("Button in header was clicked!");
+    isClicked = !isClicked;
 });
+
+//Once recording is done, open page (resembling the notification page)
+//look at transcript and ask ChatGPT to list what claims it makes
+//list deeper explanation under each claim
+//save recording stuff to pop-up 
